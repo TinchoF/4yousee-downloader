297549caf1d4f4470b74f493be0971d3db0a9208
Martin Folmer <>
2019-11-11 13:15:42 -0300

    Delete unused files

--- renderer.js
+++ renderer.js
@@ -1,6 +1,7 @@
 const remote = require('electron').remote;
 const app = remote.app;
 const appPath = app.getAppPath();
+const fs = require('fs');
 let path = appPath.replace("resources\\app.asar", '')
 console.log(path)
 let download = require('download-file');
@@ -20,7 +21,27 @@ async function downloadAd(ad){
 	let file = await createFile();
 	console.log(file)
 }
+
+async function clean(adsFiles){
+	const read = function(){
+		return new Promise((resolve,reject)=>{
+			fs.readdir(path+"/downloads/", function (err, files) {
+				if (err) {reject (err)} 
+				resolve (files)
+			});
+		});
+	}
+	let files = await read()
+	for (let i=0; i<files.length; i++){
+		if (!adsFiles.includes(files[i])){ //si el archivo no está en las ads
+			fs.unlink(path+"/downloads/"+files[i],function(error){if (error) console.log(error);}); //borra archivo
+			console.log(path+"/downloads/"+files[i]+' deleted');
+		}
+	}
+}
+
 async function downloadContent(){
+	let adsFiles = []
 	let camp = JSON.parse(await fetch("http://test.outcondigital.com:8048/campaigns/available?demo=true")
 	.then(function(response) {
 		return response.text();
@@ -35,8 +56,11 @@ async function downloadContent(){
 	for (let i=0; i<camp.length; i++){
 		for (let adi = 0; adi<camp[i].ads.length; adi++){
 			downloadAd(camp[i].ads[adi]);
+			let f = decodeURIComponent(camp[i].ads[adi].creatives[0].url).split('/')
+			adsFiles.push(f[f.length-1]);
 		}
 	}
+	clean(adsFiles);
 }
 downloadContent()
 setInterval(function(){
